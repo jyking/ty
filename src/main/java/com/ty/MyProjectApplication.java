f64967ee787ab2d3a47a45c6bf54bd88b92494e1
jyking <jyking@qq.com>
2012-04-12 15:10:31 +0800

    a

---
+++ src/main/java/com/ty/MyProjectApplication.java
@@ -0,0 +1,23 @@
+package com.ty;
+
+import java.util.Date;
+
+import com.vaadin.Application;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Window.Notification;
+
+public class MyProjectApplication extends Application {
+
+	private static final long serialVersionUID = 742133396374707487L;
+
+	@Override
+	public void init() {
+		final Window mainWindow = new Window("ty我的应用");
+		mainWindow.addComponent(new WindowOpener("Window Opener", mainWindow));
+		setMainWindow(mainWindow);
+	}
+
+}
